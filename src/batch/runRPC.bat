b784fac139a9b5f0471e9e9f7cf1be19187e5c96
Thiago Araújo <thiago.sky25@gmail.com>
2022-09-23 13:55:23 -0300

    changed exe folder

--- src/batch/runRPC.bat
+++ src/batch/runRPC.bat
@@ -4,5 +4,5 @@ echo "starting hoi4RPC..."
 start "hoi4.exe" "./hoi4.exe"
 
 @REM start "hoi4RPC.exe" "PATH TO DISCORD RPC, IN CASE IT IS ON ANOTHER FOLDER"
-start "hoi4RPC.exe" "C:\Users\%USERNAME%\Documents\Paradox Interactive\Hearts of Iron IV\discordRPC\hoi4Presence.exe"
+start "hoi4RPC.exe" "C:\Users\%USERNAME%\Documents\Paradox Interactive\Hearts of Iron IV\hoi4Presence\hoi4Presence.exe"
 exit
