c98121e249142f0e6fff34b0ed3e7077095ecb75
Thiago Araújo <thiago.sky25@gmail.com>
2022-09-23 23:09:36 -0300

    fixed wrong check update

--- src/batch/runRPC.bat
+++ src/batch/runRPC.bat
@@ -4,7 +4,7 @@ echo "starting hoi4RPC..."
 start "hoi4.exe" "./hoi4.exe"
 
 @REM Start checkupdate
-start "hoi4RPC.exe" "C:\Users\%USERNAME%\Documents\Paradox Interactive\Hearts of Iron IV\hoi4Presence\updatecheck.exe"
+start "hoi4RPC.exe" "C:\Users\%USERNAME%\Documents\Paradox Interactive\Hearts of Iron IV\hoi4Presence\checkupdate.exe"
 
 @REM start "hoi4RPC.exe" "PATH TO DISCORD RPC, IN CASE IT IS ON ANOTHER FOLDER"
 start "hoi4RPC.exe" "C:\Users\%USERNAME%\Documents\Paradox Interactive\Hearts of Iron IV\hoi4Presence\hoi4Presence.exe"
