fac64176dfd9519e867c63cfc177441fc6de6d85
Ramon <lord@equityminer.eu>
2023-05-21 09:39:20 +0200

    Update main.py
    I added all the starting nations

--- src/discordRPC/main.py
+++ src/discordRPC/main.py
@@ -9,7 +9,7 @@ import glob
 # FLAGS FOR THE COMPILER: --name hoi4Presence --noconsole --onefile
 
 try:
-   client_id = '1021549599732809820'
+   client_id = '1109742258309308477'
    RPC = Presence(client_id)  # Initialize the Presence class
    RPC.connect()  # Start the handshake loop
 
@@ -73,29 +73,251 @@ while gameRunning:  # The presence will stay on as long as the program is runnin
 
          match country:
             case "GER":
-               country = "Germany"
+               country = "German Reich"
                flag = "german_reich"
+            case "AFG":
+               country = "Afghanistan"
+               flag = "afghanistan"
+            case "ALB":
+               country = "Albania"
+               flag = "albania"
+            case "ARG":
+               country = "Argentina"
+               flag = "argentina"
+            case "AFA":
+               country = "Sultanate of Aussa"
+               flag = "aussa"
+            case "AST":
+               country = "Australia"
+               flag = "australia"
+            case "AUS":
+               country = "Austria"
+               flag = "austria"
+            case "BEL":
+               country = "Belgium"
+               flag = "belgium"
+            case "BHU":
+               country = "Bhutan"
+               flag = "bhutan"
+            case "BOL":
+               country = "Bolivia"
+               flag = "bolivia"
+            case "BRA":
+               country = "Brazil"
+               flag = "brazil"
+            case "MAL":
+               country = "British Malaya"
+               flag = "british_malaya"
+            case "RAJ":
+               country = "British Raj"
+               flag = "british_raj"
+            case "BUL":
+               country = "Bulgaria"
+               flag = "bulgaria"
+            case "CAN":
+               country = "Dominion of Canada"
+               flag = "canada"
+            case "CHL":
+               country = "Chile"
+               flag = "chile"
+            case "CHI":
+               country = "China"
+               flag = "china"
+            case "COL":
+               country = "Colombia"
+               flag = "colombia"
+            case "PRC":
+               country = "People's Republic of China"
+               flag = "prc"
+            case "COS":
+               country = "Costa Rica"
+               flag = "costa_rica"
+            case "CUB":
+               country = "Cuba"
+               flag = "cuba"
+            case "CZE":
+               country = "Czechoslovakia"
+               flag = "czechoslovakia"
+            case "DEN":
+               country = "Denmark"
+               flag = "denmark"
+            case "DOM":
+               country = "Dominican Republic"
+               flag = "dominican_republic"
+            case "INS":
+               country = "Dutch East Indies"
+               flag = "netherlands"
+            case "ECU":
+               country = "Ecuador"
+               flag = "ecuador"
+            case "ELS":
+               country = "El Salvador"
+               flag = "el_salvador"
+            case "EST":
+               country = "Estonia"
+               flag = "estonia"
+            case "ETH":
+               country = "Ethiopia"
+               flag = "ethiopia"
+            case "FIN":
+               country = "Finland"
+               flag = "finland"
+            case "FRA":
+               country = "France"
+               flag = "france"
+            case "GRE":
+               country = "Greece"
+               flag = "greece"
+            case "GXC":
+               country = "Guangxi Clique"
+               flag = "guangxi_clique"
+            case "GUA":
+               country = "Guatemala"
+               flag = "guatemala"
+            case "HAI":
+               country = "Haiti"
+               flag = "haiti"
+            case "HON":
+               country = "Honduras"
+               flag = "honduras"
+            case "HUN":
+               country = "Hungary"
+               flag = "hungary"
+            case "PER":
+               country = "Iran"
+               flag = "iran"
+            case "IRQ":
+               country = "Iraq"
+               flag = "iraq"
+            case "IRE":
+               country = "Ireland"
+               flag = "ireland"
             case "ITA":
                country = "Italy"
                flag = "italy"
             case "JAP":
                country = "Japan"
                flag = "japan"
-            case "SOV":
-               country = "Soviet Union"
-               flag = "soviet_union"
+            case "LAT":
+               country = "Latvia"
+               flag = "latvia"
+            case "LIB":
+               country = "Liberia"
+               flag = "liberia"
+            case "LIT":
+               country = "Lithuania"
+               flag = "lithuania"
+            case "LUX":
+               country = "Luxembourg"
+               flag = "luxembourg"
+            case "MAN":
+               country = "Manchukuo"
+               flag = "manchukuo"
+            case "MEN":
+               country = "Mengkukuo"
+               flag = "mengkukuo"
+            case "MEX":
+               country = "Mexico"
+               flag = "mexico"
+            case "MON":
+               country = "Mongolia"
+               flag = "mongolia"
+            case "NEP":
+               country = "Nepal"
+               flag = "nepal"
+            case "HOL":
+               country = "Netherlands"
+               flag = "netherlands"
+            case "NZL":
+               country = "New Zealand"
+               flag = "new_zealand"
+            case "NIC":
+               country = "Nicaragua"
+               flag = "nicaragua"
+            case "OMA":
+               country = "Oman"
+               flag = "aussa"
+            case "PAN":
+               country = "Panama"
+               flag = "panama"
+            case "PAR":
+               country = "Paraguay"
+               flag = "paraguay"
+            case "PRU":
+               country = "Peru"
+               flag = "peru"
+            case "PHI":
+               country = "Philippines"
+               flag = "philipines"
             case "POL":
                country = "Poland"
                flag = "poland"
-            case "FRA":
-               country = "France"
-               flag = "france"
-            case "USA":
-               country = "United States"
-               flag = "united_states"
+            case "POR":
+               country = "Portugal"
+               flag = "portugal"
+            case "ROM":
+               country = "Romania"
+               flag = "kingdom_of_romania"
+            case "SAU":
+               country = "Saudi Arabia"
+               flag = "saudiarabia"
+            case "SHX":
+               country = "Shanxi"
+               flag = "shanxi"
+            case "SIA":
+               country = "Siam"
+               flag = "siam"
+            case "SIK":
+               country = "Sinkiang"
+               flag = "sinkiang"
+            case "SAF":
+               country = "South Africa"
+               flag = "saf"
+            case "SOV":
+               country = "Soviet Union"
+               flag = "soviet_union"
+            case "SPR":
+               country = "Spain"
+               flag = "spain"
+            case "SWE":
+               country = "Sweden"
+               flag = "sweden"
+            case "SWI":
+               country = "Switzerland"
+               flag = "switzerland"
+            case "TAN":
+               country = "Tannu Tuva"
+               flag = "tannu"
+            case "TIB":
+               country = "Tibet"
+               flag = "tibet"
+            case "TUR":
+               country = "Turkey"
+               flag = "turkey"
             case "ENG":
-               country = "United Kingdom"
-               flag = "united_kingdom"
+               country = "British Empire"
+               flag = "british_empire"
+            case "USA":
+               country = "United States of America"
+               flag = "usa"
+            case "URG":
+               country = "Uruguay"
+               flag = "uruguay"
+            case "VEN":
+               country = "Venezuela"
+               flag = "venezuela"
+            case "XSM":
+               country = "Xibei San Ma"
+               flag = "shanxi"
+            case "YEM":
+               country = "Yemen"
+               flag = "yemen"
+            case "YUG":
+               country = "Yugoslavia"
+               flag = "yugoslavia"
+            case "YUN":
+               country = "Yunnan"
+               flag = "shanxi"
             case other:
                country = data[1]
                flag = "hoi4-logo"
