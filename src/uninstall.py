015b6b880eb2597a11a33cb646568f9c06d20073
ThiaudioTT <thiago.sky25@gmail.com>
2024-04-21 13:50:07 -0300

    fix(setup/uninstall): adds encoding as ut-f8

--- src/uninstall.py
+++ src/uninstall.py
@@ -54,10 +54,10 @@ while True:
 # Revert launcher-settings.json
 try:
     print("Changing launcher-settings.json...")
-    with open(gameFolder + "\\launcher-settings.json", "r") as f:
+    with open(gameFolder + "\\launcher-settings.json", "r", encoding="utf-8") as f:
         launcher = json.load(f)
         launcher["exePath"] = "hoi4.exe"
-    with open(gameFolder + "\\launcher-settings.json", "w") as f:
+    with open(gameFolder + "\\launcher-settings.json", "w", encoding="utf-8") as f:
         json.dump(launcher, f, indent=4)
 except Exception as e:
     print(e)
